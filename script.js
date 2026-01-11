68aef2953180586f8f37b4091bfe9aafdd158722
samokorochisom <158033057+samokorochisom@users.noreply.github.com>
2026-01-11 12:30:30 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// Set the countdown date
+var countdowndate = new Date("March 10, 2024 12:30:00").getTime();
+
+// Update every second
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countdowndate - now;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerText = "EXPIRED";
+    document.getElementById("days").innerText = "00";
+    document.getElementById("hours").innerText = "00";
+    document.getElementById("minutes").innerText = "00";
+    document.getElementById("seconds").innerText = "00";
+    return;
+  }
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days.toString().padStart(2, "0");
+  document.getElementById("hours").innerText = hours.toString().padStart(2, "0");
+  document.getElementById("minutes").innerText = minutes.toString().padStart(2, "0");
+  document.getElementById("seconds").innerText = seconds.toString().padStart(2, "0");
+}, 1000);
